64983186872fa0eaacdbb7db8dfc89dbf9464858
Breez17 <bogdanpronkurov@gmail.com>
2019-10-22 13:07:34 +0300

    'html'

--- js/index.js
+++ js/index.js
@@ -19,3 +19,12 @@
     nodeMenuButton: nodeMenu.querySelector('.js-menu__toggle')
   });
 })();
+
+$(document).ready(function () {
+  $("#wrap-menu__nav, #nav-holder, #menu").on("click","a", function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body, html'). animate({scrollTop: top}, 700);
+  })
+});
